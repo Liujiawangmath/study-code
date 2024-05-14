0de28630da67339f8534bc18a48d28366ab25993
Liujiawangmath <15674760037@163.com>
2024-05-14 11:42:34 +0800

    update

---
+++ FuelRodFEM/parabolic_FEM/Fuelrod2dFEMnew(o).py
@@ -0,0 +1,312 @@
+import matplotlib.pyplot as plt
+from fealpy.mesh import TriangleMesh
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from scipy.sparse.linalg import spsolve
+from fealpy.mesh import TriangleMesh
+from fealpy.functionspace import LagrangeFESpace
+from fealpy.fem import DiffusionIntegrator
+from fealpy.fem import BilinearForm
+from fealpy.fem import ScalarMassIntegrator
+from fealpy.fem import LinearForm
+from fealpy.fem import ScalarSourceIntegrator
+from fealpy.fem.dirichlet_bc import DirichletBC
+import matplotlib.pyplot as plt
+from fealpy.mesh import TriangleMesh
+import numpy as np
+def from_fuel_rod_gmsh(R1,R2,L,w,h,meshtype='normal'):
+    """
+    Generate a trangle mesh for a fuel-rod region by gmsh
+
+    @param R1 The radius of semicircles
+    @param R2 The radius of quarter circles
+    @param L The length of straight segments
+    @param w The thickness of caldding
+    @param h Parameter controlling mesh density
+    @param meshtype Choose whether to add mesh refinement at the boundary
+    @return TriangleMesh instance
+    """
+    import gmsh
+    import numpy as np
+    from fealpy.mesh import TriangleMesh
+    gmsh.initialize()
+    gmsh.model.add("fuel_rod_2D")
+
+    # 内部单元大小
+    Lc1 = h
+    # 包壳单元大小
+    Lc2 = h/2.5
+
+
+    factory = gmsh.model.geo
+    # 外圈点
+    factory.addPoint( -R1 -R2 -L, 0 , 0 , Lc2 , 1 )#圆心1
+    factory.addPoint( -R1 -R2 -L, -R1 , 0 , Lc2 , 2)
+    factory.addPoint( -R1 -R2 , -R1 , 0 , Lc2 , 3)
+    factory.addPoint( -R1 -R2 , -R1 -R2 , 0 , Lc2 , 4)#圆心2
+    factory.addPoint( -R1 , -R1 -R2 , 0 , Lc2 , 5)
+    factory.addPoint( -R1 , -R1 -R2 -L , 0 , Lc2 , 6)
+    factory.addPoint( 0 , -R1 -R2 -L , 0 , Lc2 , 7)#圆心3
+    factory.addPoint( R1 , -R1 -R2 -L , 0 , Lc2 , 8)
+    factory.addPoint( R1 , -R1 -R2 , 0 , Lc2 , 9)
+    factory.addPoint( R1 +R2 , -R1 -R2 , 0, Lc2 , 10)#圆心4
+    factory.addPoint( R1 +R2 , -R1 , 0 , Lc2 , 11) 
+    factory.addPoint( R1 +R2 +L , -R1 , 0 , Lc2 , 12)
+    factory.addPoint( R1 +R2 +L , 0 , 0 , Lc2 , 13)#圆心5
+    factory.addPoint( R1 +R2 +L , R1 , 0 , Lc2 , 14)
+    factory.addPoint( R1 +R2 , R1 , 0 , Lc2 , 15)
+    factory.addPoint( R1 +R2 , R1 +R2 , 0 , Lc2 , 16)#圆心6
+    factory.addPoint( R1 , R1 +R2 , 0 , Lc2 , 17)
+    factory.addPoint( R1 , R1 +R2 +L , 0 , Lc2 , 18)
+    factory.addPoint( 0 , R1 +R2 +L , 0 , Lc2 , 19)#圆心7
+    factory.addPoint( -R1 , R1 +R2 +L , 0 , Lc2 , 20)
+    factory.addPoint( -R1 , R1 +R2 , 0 , Lc2 , 21)
+    factory.addPoint( -R1 -R2 , R1 +R2 , 0 , Lc2 , 22)#圆心8
+    factory.addPoint( -R1 -R2 , R1 , 0 , Lc2 , 23)
+    factory.addPoint( -R1 -R2 -L , R1 , 0 , Lc2 , 24)
+
+    # 外圈线
+    line_list_out = []
+    for i in range(8):
+        if i == 0:
+            factory.addCircleArc(24 , 3*i+1 , 3*i+2, 2*i+1)
+            factory.addLine( 3*i+2 , 3*i+3 , 2*(i+1) )
+        else:
+            factory.addCircleArc(3*i , 3*i+1 , 3*i+2 , 2*i+1)
+            factory.addLine( 3*i+2 , 3*i+3 , 2*(i+1) )
+        # 填充线环中的线
+        line_list_out.append(2*i+1)
+        line_list_out.append(2*(i+1))
+    # 生成外圈线环
+    factory.addCurveLoop(line_list_out,17)
+
+    # 内圈点
+    factory.addPoint( -R1 -R2 -L, -R1 +w , 0 , Lc1 , 25)
+    factory.addPoint( -R1 -R2 , -R1 +w , 0 , Lc1 , 26)
+    factory.addPoint( -R1 +w , -R1 -R2 , 0 , Lc1 , 27)
+    factory.addPoint( -R1 +w , -R1 -R2 -L , 0 , Lc1 , 28)
+    factory.addPoint( R1 -w , -R1 -R2 -L , 0 , Lc1 , 29)
+    factory.addPoint( R1 -w , -R1 -R2 , 0 , Lc1 , 30)
+    factory.addPoint( R1 +R2 , -R1 +w , 0 , Lc1 , 31) 
+    factory.addPoint( R1 +R2 +L , -R1 +w , 0 , Lc1 , 32)
+    factory.addPoint( R1 +R2 +L , R1 -w , 0 , Lc1 , 33)
+    factory.addPoint( R1 +R2 , R1 -w , 0 , Lc1 , 34)
+    factory.addPoint( R1 -w , R1 +R2 , 0 , Lc1 , 35)
+    factory.addPoint( R1 -w , R1 +R2 +L , 0 , Lc1 , 36)
+    factory.addPoint( -R1 +w , R1 +R2 +L , 0 , Lc1 , 37)
+    factory.addPoint( -R1 +w , R1 +R2 , 0 , Lc1 , 38)
+    factory.addPoint( -R1 -R2 , R1 -w, 0 , Lc1 , 39)
+    factory.addPoint( -R1 -R2 -L , R1 -w, 0 , Lc1 , 40)
+
+    # 内圈线
+    line_list_in = []
+    for j in range(8):
+        if j == 0:
+            factory.addCircleArc(40 , 3*j+1 , 25+2*j , 18+2*j)
+            factory.addLine(25+2*j , 26+2*j , 19+2*j)
+        else:
+            factory.addCircleArc(24+2*j , 3*j+1 , 25+2*j, 18+2*j)
+            factory.addLine(25+2*j , 26+2*j , 19+2*j)
+        line_list_in.append(18+2*j)
+        line_list_in.append(19+2*j)
+    
+    def refinment():
+        gmsh.option.setNumber("Mesh.MeshSizeExtendFromBoundary", 0)
+        gmsh.option.setNumber("Mesh.MeshSizeFromPoints", 0)
+        gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", 0)
+        gmmf = gmsh.model.mesh.field
+        gmmf.add("Distance",1)
+        gmmf.setNumbers(1, "CurvesList",line_list_in)
+        gmmf.setNumber(1,"Sampling",1000)
+        gmmf.add("Threshold",2)
+        gmmf.setNumber(2, "InField", 1)
+        gmmf.setNumber(2, "SizeMin", Lc1/5)
+        gmmf.setNumber(2, "SizeMax", Lc1)
+        gmmf.setNumber(2, "DistMin", w)
+        gmmf.setNumber(2, "DistMax", 2*w)
+        gmmf.setAsBackgroundMesh(2)
+
+    # 生成内圈线环  
+    factory.addCurveLoop(line_list_in,34)
+    # 内圈面
+    factory.addPlaneSurface([34],35)
+    # 包壳截面
+    factory.addPlaneSurface([17, 34],36)
+    factory.synchronize()
+
+    if meshtype == 'refine':
+            refinment()
+    #生成网格
+    gmsh.model.mesh.generate(2)
+
+    # 获取节点信息
+    node_coords = gmsh.model.mesh.getNodes()[1]
+
+    node = np.array(node_coords, dtype=np.float64).reshape(-1, 3)[:, 0:2].copy()
+
+    # 获取三角形单元信息
+    cell_type = 2  # 三角形单元的类型编号为 2
+    cell_tags, cell_connectivity = gmsh.model.mesh.getElementsByType(cell_type)
+    cell = np.array(cell_connectivity, dtype=np.int_).reshape(-1, 3) -1
+
+    # 获得正确的节点标签
+    node_tags = np.unique(cell_connectivity)
+
+    print(f"Number of nodes: {node.shape[0]}")
+    print(f"Number of cells: {cell.shape[0]}")
+    NN = len(node)
+    isValidNode = np.zeros(NN, dtype=np.bool_)
+    isValidNode[cell] = True
+    # 去除未三角化的点
+    node = node[isValidNode]
+    idxMap = np.zeros(NN, dtype=cell.dtype)
+    idxMap[isValidNode] = range(isValidNode.sum())
+    cell = idxMap[cell]
+    # 标签到单元的映射
+    NC = len(cell)
+    tag2cidx = np.zeros(2*NC,dtype=np.int_)
+    tag2cidx[cell_tags] = np.arange(NC)
+
+    NN = len(node)
+    tag2nidx = np.zeros(2*NN,dtype=np.int_)
+    tag2nidx[node_tags] = np.arange(NN)
+    dimtags1 = gmsh.model.getEntities(1)
+    # 共享节点编号
+    cnidx = []
+    # 边界节点编号
+    bdnidx = []
+    for dim, tag in dimtags1:
+        ntags = gmsh.model.mesh.get_elements(dim,tag)[2][0]
+        idx = tag2nidx[ntags]
+        if tag < 17:
+            bdnidx.extend(idx)
+        else:
+            cnidx.extend(idx)
+    cnidx = np.unique(cnidx)
+    bdnidx = np.unique(bdnidx)
+    # 内部节点编号和包壳节点编号
+    inctags = gmsh.model.mesh.get_elements(2,35)[1][0]
+    cactags = gmsh.model.mesh.get_elements(2,36)[1][0]
+    # 内部单元
+    incidx = tag2cidx[inctags]
+    # 外部单元
+    cacidx = tag2cidx[cactags]
+    print(incidx)
+    print(cacidx)
+    #print(cell)
+    #innidx = np.unique(tag2nidx[inntags])
+    #canidx = np.unique(tag2nidx[cantags])
+
+
+    gmsh.fltk.run()
+    gmsh.finalize()
+    return TriangleMesh(node,cell),cnidx,bdnidx,incidx,cacidx
+
+
+mm = 1e-03
+#包壳厚度
+w = 0.15 * mm
+#半圆半径
+R1 = 0.5 * mm
+#四分之一圆半径
+R2 = 1 * mm
+#连接处直线段
+L = 0.575 * mm
+#内部单元大小
+h = 0.1 * mm
+mesh,cnidx,bdnidx,incidx,cacidx= from_fuel_rod_gmsh(R1,R2,L,w,h,meshtype='normal')
+print(len(cnidx))
+print(len(bdnidx))
+#print(len(innidx))
+#print(len(canidx))
+print(len(mesh.entity('node')))
+
+
+
+class ParabolicData:
+    
+    
+    def domain(self):
+        """
+        @brief 空间区间
+        """
+        return [0, 10, 0, 10]
+
+    def duration(self):
+        """
+        @brief 时间区间
+        """
+        return [0, 10]
+    
+    def source(self):
+        
+        return 0
+    
+    def dirichlet(self, p):
+        """
+        @brief 返回 Dirichlet 边界上的给定点的位移
+        @param[in] p 一个表示空间点坐标的数组
+        @return 返回位移值，这里返回常数向量 [0.0, 0.0]
+        """
+        return 500
+
+
+pde=ParabolicData()
+source=pde.source()
+node = mesh.node
+NC=mesh.number_of_cells()
+isBdNode= mesh.ds.boundary_node_flag()
+print(isBdNode.shape)
+# 时间离散
+duration = pde.duration()
+nt = 640
+tau = (duration[1] - duration[0])/nt 
+
+####全局矩阵组装####
+space=LagrangeFESpace(mesh, p=1)
+bform3=LinearForm(space)
+bform3.add_domain_integrator(ScalarSourceIntegrator(source,q=3))
+F=bform3.assembly()
+
+#组装刚度矩阵
+alpha=np.zeros(NC)
+alpha_caldding=4e-3
+alpha_inner=8e-3
+alpha[incidx]+=alpha_inner
+alpha[cacidx]+=alpha_caldding
+bform = BilinearForm(space)
+bform.add_domain_integrator(DiffusionIntegrator(alpha,q=3))
+K = bform.assembly()
+#组装质量矩阵
+bform2=BilinearForm(space)
+bform2.add_domain_integrator(ScalarMassIntegrator(q=3))
+M=bform2.assembly()
+
+p=np.zeros_like(F)
+p+=300
+
+print(p.shape)
+
+import os
+output = './result_fuelrodnew2d'
+filename = 'temp'
+# Check if the directory exists, if not, create it
+if not os.path.exists(output):
+    os.makedirs(output)
+
+for n in range(nt):
+    t = duration[0] + n*tau
+    
+    A = M + alpha_caldding*K*tau
+    b = M @ p + tau*F
+    bc = DirichletBC(space = space, gD = pde.dirichlet) 
+    # Dirichlet边界条件
+    p=spsolve(A,b)
+    p[isBdNode] = pde.dirichlet(node[isBdNode])
+    mesh.nodedata['temp'] = p.flatten('F')
+    name = os.path.join(output, f'{filename}_{n:010}.vtu')
+    mesh.to_vtk(fname=name)
+
+print(p)
+print(p.shape)
