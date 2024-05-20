5df02685d39543461ac9a12cf7a4d23cd7ec8154
Liujiawangmath <15674760037@163.com>
2024-05-20 09:38:19 +0800

    update

--- FuelRodFEM/parabolic_FEM/solver_test.py
+++ FuelRodFEM/parabolic_FEM/solver_test.py
@@ -1,33 +1,81 @@
 import numpy as np
 import os
 from scipy.sparse.linalg import spsolve
-from fealpy.mesh import TriangleMesh
+from fealpy.mesh import TriangleMesh,TetrahedronMesh
 from fealpy.functionspace import LagrangeFESpace
 from fealpy.fem import DiffusionIntegrator, BilinearForm, ScalarMassIntegrator, LinearForm, ScalarSourceIntegrator
 from fealpy.fem.dirichlet_bc import DirichletBC
+import matplotlib.pyplot as plt
 
-class ParabolicData:
+
+class Parabolic2dData:
     def domain(self):
         return [0, 10, 0, 10]
 
     def duration(self):
-        return [0, 10]
+        return [0, 0.1]
     
-    def source(self):
-        return 0
     
     def solution(self,p,t):
         pi = np.pi
         x = p[..., 0]
         y = p[..., 1]
         return np.sin(pi*x)*np.sin(pi*y)*np.exp(-2*pi*t) 
+    
+    def source(self, p, t):
+        """
+        @brief 方程右端项 
+
+        @param[in] p numpy.ndarray, 空间点
+        @param[in] t float, 时间点 
+
+        @return 方程右端函数值
+        """
+        pi = np.pi
+        x = p[..., 0]
+        y = p[..., 1]
+        return np.zeros(x.shape)
+
      
-    def dirichlet(self, p):
+    def dirichlet(self, p,t):
         
-        return 500
+        return self.solution(p,t)
+    
+class Parabolic3dData:
+    def domain(self):
+        return [0, 1, 0, 1, 0, 1]
+
+    def duration(self):
+        return [0, 1]
+
+    def source(self):
+        return 0
+
+    def solution(self, p, t):
+        pi = np.pi
+        x = p[..., 0]
+        y = p[..., 1]
+        z = p[..., 2]
+        return np.sin(pi * x) * np.sin(pi * y) * np.sin(pi * z) * np.exp(-3 * pi * t)
+
+    def dirichlet(self, p, t):
+        return self.solution(p, t)
+    
+class FuelRod3dData:
+    def domain(self):
+        return [0, 1, 0, 1, 0, 1]
+
+    def duration(self):
+        return [0, 1]
+
+    def source(self):
+        return 0
+
+    def dirichlet(self, p, t):
+        return np.array([500])
 
 class HeatEquationSolver:
-    def __init__(self, mesh: TriangleMesh, pde: ParabolicData, nt, bdnidx, p0, alpha_caldding=4e-4, alpha_inner=8e-4, layered=True, ficdx=None ,cacidx=None,output: str = './result', filename: str = 'temp'):
+    def __init__(self, mesh: TriangleMesh, pde: Parabolic2dData, nt, bdnidx, p0, alpha_caldding=4e-4, alpha_inner=8e-4, layered=True, ficdx=None ,cacidx=None,output: str = './result', filename: str = 'temp'):
         """
         Args:
             mesh (TriangleMesh): 三角形网格
@@ -46,6 +94,7 @@ class HeatEquationSolver:
         self.output = output
         self.filename = filename
         self.space = LagrangeFESpace(mesh, p=1)
+        self.GD = self.space.geo_dimension()
         self.duration = pde.duration()
         self.nt = nt
         self.p0 = p0
@@ -55,7 +104,6 @@ class HeatEquationSolver:
         self.alpha_caldding = alpha_caldding
         self.alpha_inner = alpha_inner
         self.initialize_output_directory()
-        self.assemble_matrices()
         self.threshold = self.create_threshold()
 
     def initialize_output_directory(self):
@@ -74,77 +122,142 @@ class HeatEquationSolver:
             isbdnidx[self.bdnidx] = True
             return isbdnidx
 
-    def assemble_matrices(self):
-        # 全局矩阵组装
-        source = self.pde.source()
-        bform3 = LinearForm(self.space)
-        bform3.add_domain_integrator(ScalarSourceIntegrator(source, q=3))
-        self.F = bform3.assembly()
-
-        # 组装刚度矩阵
-        NC = self.mesh.number_of_cells()
-        alpha = np.zeros(NC)
-        
-        if self.layered:
-            # 假设 ficdx 和 cacidx 是定义好的两个索引列表
-            # 默认分层
-            alpha[self.ficdx] += self.alpha_inner
-            alpha[self.cacidx] += self.alpha_caldding
-        else:
-            # 如果不分层，使用统一的 alpha_caldding
-            alpha += self.alpha_caldding
-
-        bform = BilinearForm(self.space)
-        bform.add_domain_integrator(DiffusionIntegrator(alpha, q=3))
-        self.K = bform.assembly()
-
-        # 组装质量矩阵
-        bform2 = BilinearForm(self.space)
-        bform2.add_domain_integrator(ScalarMassIntegrator(q=3))
-        self.M = bform2.assembly()
-
-        # self.p = np.zeros_like(self.F)
-        self.p = self.space.function()
-        self.p += self.p0
-
-    def apply_boundary_conditions(self, A, b, t):
-        # isBdDof = self.space.is_boundary_dof()
-        # ipoints = self.space.interpolation_points()
-        #isBdNode = self.mesh.ds.boundary_node_flag()
-        #gD = lambda p: pde.dirichlet(p, t)
-        #self.p[isBdNode] = gD(isBdNode)
-
-        #b = b - A@self.p 
-        #b[isBdNode] = b[isBdNode]
-        
-        #from scipy.sparse import spdiags
-        #bdIdx = np.zeros(A.shape[0], dtype=self.mesh.itype)
-        #bdIdx[isBdNode] = 1
-        #D0 = spdiags(1 - bdIdx, 0, A.shape[0], A.shape[0])
-        #D1 = spdiags(bdIdx, 0, A.shape[0], A.shape[0])
-        #A = D0 @ A @ D0 + D1
-        
-        bc = DirichletBC(space=self.space, gD=self.pde.dirichlet, threshold=self.threshold)
-        A, b = bc.apply(A, b)
-        return A, b
 
     def solve(self):
         for n in range(self.nt):
             t = self.duration[0] + n * self.tau
+    
+            bform3 = LinearForm(self.space)
+            # 这里应该传入后的得到的为qc
+            f=self.pde.source(node,t)
+            bform3.add_domain_integrator(ScalarSourceIntegrator(f, q=3))
+            self.F = bform3.assembly()
+            
+            # 组装刚度矩阵
+            NC = self.mesh.number_of_cells()
+            alpha = np.zeros(NC)
+        
+            if self.layered:
+                # 假设 ficdx 和 cacidx 是定义好的两个索引列表
+                # 默认分层
+                alpha[self.ficdx] += self.alpha_inner
+                alpha[self.cacidx] += self.alpha_caldding
+            else:
+                # 如果不分层，使用统一的 alpha_caldding
+                alpha += self.alpha_caldding
+
+            bform = BilinearForm(self.space)
+            bform.add_domain_integrator(DiffusionIntegrator(alpha, q=3))
+            self.K = bform.assembly()
+
+            # 组装质量矩阵
+            bform2 = BilinearForm(self.space)
+            bform2.add_domain_integrator(ScalarMassIntegrator(q=3))
+            self.M = bform2.assembly()
+
+            self.p = np.zeros_like(self.F)
+            self.p += self.p0           
             A = self.M + self.alpha_caldding * self.K * self.tau
             b = self.M @ self.p + self.tau * self.F
             if n == 0:
                 A = A
                 b = b
             else:
-                A, b = self.apply_boundary_conditions(A, b, t)
+                bc = DirichletBC(space=self.space, gD=self.pde.dirichlet(self.p,t), threshold=self.threshold)
+                A, b = bc.apply(A, b)
             self.p = spsolve(A, b)
+            print(self.p)
             self.mesh.nodedata['temp'] = self.p.flatten('F')
             name = os.path.join(self.output, f'{self.filename}_{n:010}.vtu')
             self.mesh.to_vtk(fname=name)
         print(self.p)
         print(self.p.shape)
+        
+    def plot_exact_solution(self):
+        t = self.duration[1]
+        exact_solution = self.pde.solution(self.mesh.node, t)
+        print(exact_solution)
 
+        if self.GD == 2:
+            plt.figure(figsize=(8, 6))
+            plt.scatter(self.mesh.node[:, 0], self.mesh.node[:, 1], c=exact_solution, cmap='viridis', s=10)
+            plt.colorbar()
+            plt.title('Exact Solution Scatter Plot')
+            plt.xlabel('x')
+            plt.ylabel('y')
+            plt.tight_layout()
+            plt.show()
+        elif self.GD == 3:
+            fig = plt.figure(figsize=(10, 8))
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(self.mesh.node[:, 0], self.mesh.node[:, 1], self.mesh.node[:, 2], c=exact_solution, cmap='viridis', s=10)
+            plt.colorbar(ax.collections[0], ax=ax)
+            ax.set_title('Exact Solution Scatter Plot')
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            ax.set_zlabel('z')
+            plt.tight_layout()
+            plt.show()
+
+    def plot_error(self):
+        t = self.duration[1]
+        exact_solution = self.pde.solution(self.mesh.node, t)
+        numerical_solution = self.p.flatten('F')
+        error = np.abs(exact_solution - numerical_solution)
+        print(error)
+
+        if self.GD == 2:
+            plt.figure(figsize=(8, 6))
+            plt.scatter(self.mesh.node[:, 0], self.mesh.node[:, 1], c=error, cmap='viridis', s=10)
+            plt.colorbar()
+            plt.title('Error Scatter Plot')
+            plt.xlabel('x')
+            plt.ylabel('y')
+            plt.tight_layout()
+            plt.show()
+        elif self.GD == 3:
+            fig = plt.figure(figsize=(10, 8))
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(self.mesh.node[:, 0], self.mesh.node[:, 1], self.mesh.node[:, 2], c=error, cmap='viridis', s=10)
+            plt.colorbar(ax.collections[0], ax=ax)
+            ax.set_title('Error Scatter Plot')
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            ax.set_zlabel('z')
+            plt.tight_layout()
+            plt.show()
+
+"""
+# 使用示例:三维的燃料棒slover
+if __name__ == "__main__":
+    mm = 1e-03
+    #包壳厚度
+    w = 0.15 * mm
+    #半圆半径
+    R1 = 0.5 * mm
+    #四分之一圆半径
+    R2 = 1.0 * mm
+    #连接处直线段
+    L = 0.575 * mm
+    #内部单元大小
+    h = 0.5 * mm
+    #棒长
+    l = 20 * mm
+    #螺距
+    p = 40 * mm
+
+from app.FuelRodSim.fuel_rod_mesher import FuelRodMesher 
+mesher = FuelRodMesher(R1,R2,L,w,h,l,p,meshtype='segmented',modeltype='3D')
+mesh = mesher.get_mesh
+ficdx,cacidx = mesher.get_3D_fcidx_cacidx()
+cnidx,bdnidx = mesher.get_3D_cnidx_bdnidx()
+pde = FuelRod3dData()
+FuelRodsolver = HeatEquationSolver(mesh, pde,64, bdnidx,300,ficdx=ficdx,cacidx=cacidx,output='./result_fuelrod3Dtest')
+FuelRodsolver.solve()
+"""
+
+"""
+#使用示例，三维箱子热传导
 if __name__ == "__main__":
     nx = 20
     ny = 20
@@ -153,6 +266,36 @@ from fealpy.mesh import TetrahedronMesh
 mesh = TetrahedronMesh.from_box([0, 1, 0, 1, 0, 1],nx,ny,nz)
 node = mesh.node
 isBdNode = mesh.ds.boundary_node_flag()
-pde = ParabolicData()
-Boxslover = HeatEquationSolver(mesh,pde,64,isBdNode,300,alpha_caldding=1.98e-2,layered=False,output='./rusult_boxtest')
+pde = FuelRod3dData()
+Boxslover = HeatEquationSolver(mesh,pde,64,isBdNode,300,alpha_caldding=0.08,layered=False,output='./rusult_boxtest')
 Boxslover.solve()
+"""
+
+
+# 二维带真解的测试案例
+pde=Parabolic2dData()
+nx = 5
+ny = 5
+mesh = TriangleMesh.from_box([0, 1, 0, 1], nx,ny)
+node = mesh.node
+isBdNode = mesh.ds.boundary_node_flag()
+Box2dslover = HeatEquationSolver(mesh,pde,160,isBdNode,0,alpha_caldding=1,layered=False,output='./rusult_box2dtest')
+Box2dslover.solve()
+Box2dslover.plot_exact_solution() # 绘制真解
+Box2dslover.plot_error()
+
+
+"""
+# 三维带真解的测试
+pde=Parabolic3dData()
+nx = 5
+ny = 5
+nz = 5
+mesh = TetrahedronMesh.from_box([0, 1, 0, 1, 0, 1], nx, ny, nz)
+node = mesh.node
+isBdNode = mesh.ds.boundary_node_flag()
+Box3DSolver = HeatEquationSolver(mesh, pde, 160, isBdNode, 0, alpha_caldding=1, layered=False, output='./result_box3dtest')
+Box3DSolver.solve()
+Box3DSolver.plot_exact_solution()
+Box3DSolver.plot_error()
+"""
